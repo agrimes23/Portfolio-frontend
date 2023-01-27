8da25ce90c9395c1b7cbee16958025b515cbdb78
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-01-26 18:19:24 -0600

    added a temporary pic to the homepage:

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,12 @@
 import {useState, useEffect} from 'react'
 import { Routes, Link, Route, useLocation } from 'react-router-dom'
 
+
 const HomePage = () => {
 
     return (
         <>
-            <div className="style-border fadeInTop">
+            <div className="style-border">
                 <div className="intro-styling"></div>
                 <div id="intro_section" className="main-intro text-center">
                     <h1 className="main-title">Alex Grimes</h1>
