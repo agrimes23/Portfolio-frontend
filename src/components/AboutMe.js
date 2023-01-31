de66b9283e4332b2f1df77847a5afe04a29f6cbc
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-01-30 20:30:07 -0600

    added more info in the about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,8 +11,13 @@ const AboutMe = () => {
                             <h2 className="mb-4">About Me</h2>
                         </div>
                         <div>
-                            <h5 className="mt-4">Organized, results-driven Full-Stack Developer. My time working in Japan has taught me the value of teamwork and cooperation to get a project done. As well as being a dependable member of a team, I am very focused and organized which motivates me to take on ambitious projects. 
-                            </h5>
+                            <h5 className="mt-4">Full-Stack Developer trained in a 400+ hour Software Engineering course through General Assembly. I have become very familiar with React, Node.js, Python (Django), including many more frameworks and languages. </h5>
+                            <br/>
+                            <h5>My previous job as an accountant was more than accounting work. One of my projects was setting up an online expense system and training employees, as well as creating a MS Work Automate program that would send employee's submitted pre-trip business applications to their specific approvers. I took the time to review all the questions on the paper-based form and narrowed it down to 10 questions on MS Forms. The outcome was very successful; it reduced the amount of time employees spent on sending pre-trip business applications, quicker time submitting to their approvers, and of course all online.</h5>
+                            <br/>
+                            <h5>Ever since I took on the challenge to learn how to write programs through MS Automate, I have been excited to see what more I can do to help reduce time spent on tasks that need to be done. </h5>
+                            <br/>
+                            <h5>I'm a thorough thinker, organized, and very ambitious when it comes to coding projects. </h5>
                         </div>
                     </div>
                 </div>
