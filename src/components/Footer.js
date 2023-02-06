6eb56f092d432f4c3a79f278fc3328c113338cd3
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-02-06 00:22:35 -0600

    fixed footer icon issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <>
             <footer className="footer fixed-bottom p-5 my-5 text-center">
-                    <a className="py-5" href="https://github.com/agrimes23"><i className="fa-brands fa-github text-white px-5" title="Github"></i></a>
-                    <a className="py-5" href="https://www.linkedin.com/in/alex-grimes-dev/"><i className="fa-brands fa-linkedin text-white px-5" title="LinkedIn"></i></a>
-                    <a className="py-5" href="mailto: alexgrimes23@gmail.com"><i className="fa-regular fa-envelope text-white px-5" title="Email"></i></a>
+                    <a className="py-5" href="https://github.com/agrimes23"><i className="footer-text fa-brands fa-github text-white px-5" title="Github"></i></a>
+                    <a className="py-5" href="https://www.linkedin.com/in/alex-grimes-dev/"><i className="footer-text fa-brands fa-linkedin text-white px-5" title="LinkedIn"></i></a>
+                    <a className="py-5" href="mailto: alexgrimes23@gmail.com"><i className="footer-text fa-regular fa-envelope text-white px-5" title="Email"></i></a>
             </footer>
         
         </>
