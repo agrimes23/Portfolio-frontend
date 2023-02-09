9dfe7fac1d051cacc88f8589594eeec949c2367f
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-02-08 21:13:29 -0600

    added resume pdf to site. Need to add a download button and format it for mobile

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -3,7 +3,7 @@ import { Routes, Link, Route, useLocation } from 'react-router-dom'
 import ExperienceContent from './ExperienceContent'
 import styled from 'styled-components'
 import Footer from '../Footer.js'
-
+import Resume from '../Resume.js'
 
 const Experience = () => {
 
@@ -62,9 +62,11 @@ const Experience = () => {
                     </div>
                 </div>
               </div>
-
+              <div className="d-flex align-items-center justify-content-center">
+                <Resume />
+              </div>
             </div>
-            <footer className="footer-exp p-5 mb-5 text-center">
+            <footer className="footer footer-exp p-5 mb-5 text-center">
                   <a className="py-5" href="https://github.com/agrimes23"><i className="footer-text fa-brands fa-github text-white px-5" title="Github"></i></a>
                   <a className="py-5" href="https://www.linkedin.com/in/alex-grimes-dev/"><i className="footer-text fa-brands fa-linkedin text-white px-5" title="LinkedIn"></i></a>
                   <a className="py-5" href="mailto: alexgrimes23@gmail.com"><i className="footer-text fa-regular fa-envelope text-white px-5" title="Email"></i></a>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,18 +1,18 @@
 import {useState, useEffect} from 'react'
 import { Routes, Link, Route, useLocation } from 'react-router-dom'
 
-import { Document, Page } from 'react-pdf';
-import resumePDF from './images/alex_grimes_pdf.pdf'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import resumePDF from '../images/alex_grimes_pdf.pdf'
 
 
 const Resume = () => {
+
+  
     return (
-        <>
-            <Document file={resumePDF}>
-                <Page pageNumber={1} />
-            </Document>
-        </>
-    )
-}
+      <Document file={resumePDF}>
+          <Page pageNumber={1} />
+      </Document>
+    );
+  }
 
 export default Resume;
