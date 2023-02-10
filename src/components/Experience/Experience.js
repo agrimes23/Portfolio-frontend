1528ec2d1fbabb9b91ccb1d678d86c929942fd6d
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-02-09 22:04:34 -0600

    commented out the whole resume component. Fixed the footer to get site to run on heroku

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -65,7 +65,7 @@ const Experience = () => {
               <div className="m-5 resume-div d-flex row align-items-center justify-content-center">
                 {/* <Resume /> */}
               </div>
-              <footer className="footer footer-exp p-5 mb-5 text-center">
+              <footer className="footer footer-exp p-5 text-center">
                   <a className="py-5" href="https://github.com/agrimes23"><i className="footer-text fa-brands fa-github text-white px-5" title="Github"></i></a>
                   <a className="py-5" href="https://www.linkedin.com/in/alex-grimes-dev/"><i className="footer-text fa-brands fa-linkedin text-white px-5" title="LinkedIn"></i></a>
                   <a className="py-5" href="mailto: alexgrimes23@gmail.com"><i className="footer-text fa-regular fa-envelope text-white px-5" title="Email"></i></a>
