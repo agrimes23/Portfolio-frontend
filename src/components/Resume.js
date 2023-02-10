364d7d6e6fa498485beeda7b0480cb0d7985329c
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-02-09 19:39:58 -0600

    added a download button to download my resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,12 +7,34 @@ import resumePDF from '../images/alex_grimes_pdf.pdf'
 
 const Resume = () => {
 
-  
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch(resumePDF).then(response => {
+        response.blob().then(blob => {
+            // Creating new object of PDF file
+            const fileURL = window.URL.createObjectURL(blob);
+            // Setting various property values
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = resumePDF;
+            alink.click();
+        })
+    })
+}
     return (
-      <Document file={resumePDF}>
-          <Page pageNumber={1} />
-      </Document>
-    );
+      <>
+        <div className="d-flex row">
+          <div className="d-flex justify-content-center my-5">
+            <button className ="btn btn-dwnld" onClick={onButtonClick}>Download Resume</button>      
+          </div>
+          <div className="d-flex justify-content-center">
+            <Document file={resumePDF}>
+                <Page pageNumber={1} />
+            </Document>
+          </div>
+        </div>
+      </>
+    )
   }
 
 export default Resume;
