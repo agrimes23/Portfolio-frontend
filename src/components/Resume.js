947c8e9b3449e204c44feeccddb9928729790eb9
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-02-09 20:19:01 -0600

    seeing if heroku will accept this change

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 import { Routes, Link, Route, useLocation } from 'react-router-dom'
 
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
 import resumePDF from '../images/alex_grimes_pdf.pdf'
 
 
