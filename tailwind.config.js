1be10ec3036c134bf2c90d6737c923b221c8b936
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-23 09:54:45 -0500

    set up screen breakpoints, might change later. Will start changing media queries to tw css

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
       'darkGreen': '#022627',
       'darkBrown': '#333333',
       'codingDiv': '#272727',
+      'darkRed': '#8B0000',
+    },
+    screens: {
+      '2xlMax': {'max': '1110px'},
+      '2xlMin': {'min': '1110px'}, // do I need mins?
+      'xlMax': {'max': '900px'},
+      'xlMin': {'min': '900px'}, // do I need mins?
+      'lgMax': {'max': '755px'},
+      'lgMin': {'min': '755px'}, // do I need mins?
+      'mdMax': {'max': '600px'},
+      'smMax': {'max': '500px'},
     },
     extend: {},
   },
