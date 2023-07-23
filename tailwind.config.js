613ed80a489755f4100701facaa4c326dc410c19
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-22 20:32:48 -0500

    imported font family and font colors to the tailwind config, applied them to the main section of the home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      titleName: ['Black Han Sans']
+    },
+    colors: {
+      'darkGreen': '#022627',
+    },
     extend: {},
   },
   plugins: [],
