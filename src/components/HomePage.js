46224949f36433c75c347e0bffb076104f81f0d7
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-22 21:33:12 -0500

    fixed font sizes on home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -26,7 +26,7 @@ const HomePage = () => {
             {/* projects section with video mirroring */}
             <div className="projects-hp bg-codingDiv h-100">
                 <div className="col mx-auto absolute pos-forward w-100 flex justify-center">
-                    <Link title="Alex Grimes' Coding Projects" className="text-box mb-0 justify-center items-center no-underline text-white" to="/projects"><h2 className="bg-darkBrown bg-opacity-95 flex items-center p-5">View Alex's Coding Projects</h2></Link>
+                    <Link title="Alex Grimes' Coding Projects" className="text-box mb-0 justify-center items-center no-underline text-white" to="/projects"><h2 className="bg-darkBrown text-3xl bg-opacity-95 flex items-center p-5">View Alex's Coding Projects</h2></Link>
                 </div>
                 {/* try to make it look like a reflection on either side of the video */}
                 <div className="w-100 video-section flex justify-center py-5">
@@ -47,7 +47,7 @@ const HomePage = () => {
                 <div className="exp-conatiner-hp m-auto mx-3">
                     <Link title="Alex Grimes' Work Experience" className="no-underline text-white" to="/experience">
                         <div className="experience-hp flex justify-center items-center p-3">
-                            <h2 className="experience-title-hp">Read about how Alex became a Programmer and Other Experiences</h2>
+                            <h2 className="experience-title-hp text-3xl">Read about how Alex became a Programmer and Other Experiences</h2>
                         </div> 
                     </Link>
                 </div>
@@ -56,7 +56,7 @@ const HomePage = () => {
                 <div className="exp-conatiner-hp m-auto mx-3">
                     <Link title="Alex Grimes' About Me Page" className="no-underline text-black" to="/aboutme">
                         <div className="about-me-hp flex justify-center items-center p-3">
-                            <h2 className="about-me-title-hp">Still Curious? Read the About Me Page!</h2>
+                            <h2 className="about-me-title-hp text-3xl">Still Curious? Read the About Me Page!</h2>
                         </div> 
                     </Link>
                 </div>
