e8d747d9f356211d7af17d8dfd2c7eaabefba715
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-21 20:57:30 -0500

    fixing up about me page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -30,7 +30,7 @@ const HomePage = () => {
                     
 
             {/* projects section with video mirroring */}
-            {/* <div className="projects-hp mdMax:h-64 bg-codingDiv h-100">
+            <div className="projects-hp my-10 mdMax:h-64 bg-codingDiv h-100">
                 <div className="col mx-auto absolute pos-forward smMax:translate-y-2/4 mdMax:translate-y-3/4 xlMax:translate-y-full z-10 translate-y-full w-100 flex justify-center">
                     <Link title="Alex Grimes' Coding Projects" className="text-box mb-0 justify-center items-center no-underline text-white" to="/projects"><h2 className="bg-darkBrown text-3xl bg-opacity-95 flex items-center p-5">View Alex's Coding Projects</h2></Link>
                 </div>
@@ -46,7 +46,7 @@ const HomePage = () => {
                             <source src={codingVidBG} type="video/mp4" />
                     </video>
                 </div>
-            </div> */}
+            </div>
 
             {/* about me */}
             <div className="exp-abtme-container flex xlMin:flex-row flex-col mb-5">
