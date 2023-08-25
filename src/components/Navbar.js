7c6f0e867e77ce874d806abba6242a6a1c8cb178
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-25 10:59:54 -0500

    resume download button works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import { Routes, Link, Route, useLocation, useNavigate } from 'react-router-dom'
+import Resume from "../assets/Alexandra_Grimes_Resume.pdf"
 
 const Navbar = () => {
 
@@ -47,7 +48,7 @@ const Navbar = () => {
             </div>
             <div className="lg-screen-nav flex 2xlMax:hidden justify-content-end">
                 <ul className="d-flex flex-row navbar-nav navbar-right text-white mx-3">
-                    <div className="mt-2"><label htmlFor="download resume"><a className="text-decoration-none button py-3 px-3 mx-4" href="../images/Alexandra_Grimes_Resume.PDF" download="Alexandra_Grimes_Resume.PDF">Download Resume</a></label>
+                    <div className="mt-2"><label htmlFor="download resume"><a className="text-decoration-none button py-3 px-3 mx-4" href={Resume} download="Alexandra_Grimes_Resume.pdf" target="_blank">Download Resume</a></label>
                     </div>
                     <label htmlFor="about me" className="nav-item-area pt-2"><Link className="text-decoration-none nav-item p-4" to='/aboutme'>About Me</Link></label>
                     <label htmlFor="web projects" className="nav-item-area pt-2"><Link className="text-decoration-none nav-item p-4" to='/webprojects'>Web Projects</Link></label>
@@ -67,7 +68,7 @@ const Navbar = () => {
                     <a className="sm-nav-item text-decoration-none m-auto text-white p-4" onClick={redirectWP}>Web Projects</a>
                     <a className="sm-nav-item text-decoration-none m-auto text-white p-4" onClick={redirectMP}>Mobile Projects</a>
                     <a className="sm-nav-item text-decoration-none m-auto text-white p-4" onClick={redirectE}>Experience</a>
-                    <a className="sm-nav-item text-decoration-none m-auto text-white p-4" download={"../images/Alexandra_Grimes_Resume.PDF"}>Download Resume</a>
+                    <a className="sm-nav-item text-decoration-none m-auto text-white p-4" href={Resume} download="Alexandra_Grimes_Resume" target='_blank'>Download Resume</a>
 
                     
                 </div>
