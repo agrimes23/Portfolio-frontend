3a7d548a6245799b22a342f80cf0b449a7c62e84
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-27 16:03:04 -0500

    fixed up nav bar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,41 +38,93 @@ const Navbar = () => {
 
   return (
     <>
-        <nav className="navbar sticky-top p-4 d-flex columnnavbar">
-            <div className="flex flex-row">
-                <Link className="text-decoration-none" to='/'><h1 className="nav-home" title="Home Page" alt="logo-map-planner-img">AG</h1></Link>
+      <nav className="navbar sticky-top p-4 d-flex columnnavbar">
+        <div className="flex flex-row">
+          <button className="text-decoration-none">
+            <h1
+              className="nav-home"
+              title="Home Page"
+              alt="logo-map-planner-img"
+            >
+              AG
+            </h1>
+          </button>
+        </div>
+        <div className="lg-screen-nav flex 2xlMax:hidden justify-end">
+          <div className="flex flex-row navbar-nav navbar-right text-white mx-3">
+            <div className="my-auto">
+              <label htmlFor="download resume" />
+              <a
+                className="text-decoration-none button py-3 px-3 mx-4"
+                href={Resume}
+                download="Alexandra_Grimes_Resume.pdf"
+                target="_blank"
+              >
+                Download Resume
+              </a>
             </div>
-            <div className="lg-screen-nav flex 2xlMax:hidden justify-end">
-                <ul className="flex flex-row navbar-nav navbar-right text-white mx-3">
-                    <div className="mt-2"><label htmlFor="download resume"><a className="text-decoration-none button py-3 px-3 mx-4" href={Resume} download="Alexandra_Grimes_Resume.pdf" target="_blank">Download Resume</a></label>
-                    </div>
-                    <label htmlFor="about me" className="nav-item-area pt-2"><Link className="text-decoration-none nav-item p-4" to='/aboutme'>About Me</Link></label>
-                    <label htmlFor="web projects" className="nav-item-area pt-2"><Link className="text-decoration-none nav-item p-4" to='/webprojects'>Web Projects</Link></label>
-                    <label htmlFor="web projects" className="nav-item-area pt-2"><Link className="text-decoration-none nav-item p-4" to='/mobileprojects'>Mobile Projects</Link></label>
-                    <label htmlFor="experience" className="nav-item-area pt-2"><Link className="text-decoration-none nav-item p-4" to='/experience'>Experience</Link></label>
-                </ul>
-            </div>
-            <div className="sm-screen-nav hidden 2xlMax:flex">
-                <a onClick={setDisplayTab}>
-                    <i className="fa-solid fa-bars"/>
-                </a>
-            </div>
-        </nav>
-            <div className={showTab}>
-                <div className="ham-tab hidden 2xlMax:flex NavTop flex-col py-5 text-center">
-                    <a className="sm-nav-item text-decoration-none m-auto text-white p-4" onClick={redirectAB}>About Me</a>
-                    <a className="sm-nav-item text-decoration-none m-auto text-white p-4" onClick={redirectWP}>Web Projects</a>
-                    <a className="sm-nav-item text-decoration-none m-auto text-white p-4" onClick={redirectMP}>Mobile Projects</a>
-                    <a className="sm-nav-item text-decoration-none m-auto text-white p-4" onClick={redirectE}>Experience</a>
-                    <a className="sm-nav-item text-decoration-none m-auto text-white p-4" href={Resume} download="Alexandra_Grimes_Resume" target='_blank'>Download Resume</a>
-
-                    
-                </div>
-            </div>
-
-        
+            <label htmlFor="about me"/>
+              <button className="nav-item nav-item-area p-4">
+                About Me
+              </button>
+            <label htmlFor="web projects"/>
+              <button className="nav-item nav-item-area p-4">
+                Web Projects
+              </button>
+            <label htmlFor="web projects"/>
+              <button className="nav-item nav-item-area p-4">
+                Mobile Projects
+              </button>
+            <label htmlFor="experience"/>
+              <button className="nav-item nav-item-area p-4">
+                Experience
+              </button>
+          </div>
+        </div>
+        <div className="sm-screen-nav hidden 2xlMax:flex">
+          <a onClick={setDisplayTab}>
+            <i className="fa-solid fa-bars" />
+          </a>
+        </div>
+      </nav>
+      <div className={showTab}>
+        <div className="ham-tab hidden 2xlMax:flex NavTop flex-col py-5 text-center">
+          <a
+            className="sm-nav-item text-decoration-none m-auto text-white p-4"
+            onClick={redirectAB}
+          >
+            About Me
+          </a>
+          <a
+            className="sm-nav-item text-decoration-none m-auto text-white p-4"
+            onClick={redirectWP}
+          >
+            Web Projects
+          </a>
+          <a
+            className="sm-nav-item text-decoration-none m-auto text-white p-4"
+            onClick={redirectMP}
+          >
+            Mobile Projects
+          </a>
+          <a
+            className="sm-nav-item text-decoration-none m-auto text-white p-4"
+            onClick={redirectE}
+          >
+            Experience
+          </a>
+          <a
+            className="sm-nav-item text-decoration-none m-auto text-white p-4"
+            href={Resume}
+            download="Alexandra_Grimes_Resume"
+            target="_blank"
+          >
+            Download Resume
+          </a>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Navbar;
