e03eb48dd73ae078ea5e0d0e8e783758f008e699
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-28 21:28:34 -0500

    testing out wood image as footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import {useState, useEffect} from 'react'
 import { Routes, Link, Route, useLocation } from 'react-router-dom'
+import wood from '../assets/wood.jpg'
 
 const Footer = () => {
     return (
         <>
-            <footer className="bg-darkSalmon p-5 text-center">
+            <footer className="bg-[#3f2b17] p-5 text-center bg-cover" style={{ backgroundImage: `url(${wood})` }} >
                     <a className="py-5" href="https://github.com/agrimes23"><i className="footer-text fa-brands fa-github text-white px-5" title="Github"></i></a>
                     <a className="py-5" href="https://www.linkedin.com/in/alex-grimes-dev/"><i className="footer-text fa-brands fa-linkedin text-white px-5" title="LinkedIn"></i></a>
                     <a className="py-5" href="mailto: alexgrimes23@gmail.com"><i className="footer-text fa-regular fa-envelope text-white px-5" title="Email"></i></a>
