d2f55dd8c76664a728c341839638d6d5d7715896
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-30 16:30:14 -0500

    styling page with dark purple and light blue water colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import wood from '../assets/wood.jpg'
 const Footer = () => {
     return (
         <>
-            <footer className="bg-[#3f2b17] p-5 text-center bg-cover" style={{ backgroundImage: `url(${wood})` }} >
+            <footer className="bg-[#0e2038] p-5 text-center bg-cover" >
                     <a className="py-5" href="https://github.com/agrimes23"><i className="footer-text fa-brands fa-github text-white px-5" title="Github"></i></a>
                     <a className="py-5" href="https://www.linkedin.com/in/alex-grimes-dev/"><i className="footer-text fa-brands fa-linkedin text-white px-5" title="LinkedIn"></i></a>
                     <a className="py-5" href="mailto: alexgrimes23@gmail.com"><i className="footer-text fa-regular fa-envelope text-white px-5" title="Email"></i></a>
